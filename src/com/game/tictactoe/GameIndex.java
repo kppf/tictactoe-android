f6015e45ac3d849ce0531f3df702754c9b567cfe
Kushagra Pandey <kushagra4296@gmail.com>
2013-01-25 22:31:55 +0530

    Yeah!

---
+++ src/com/game/tictactoe/GameIndex.java
@@ -0,0 +1,22 @@
+package com.game.tictactoe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GameIndex extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_index);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_game_index, menu);
+		return true;
+	}
+
+}
