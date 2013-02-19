4273548e854f19452b21ffc3de48efb4fb8b9c8d
Kushagra Pandey <kushagra4296@gmail.com>
2013-02-19 20:20:07 +0530

    the restart with the hell

---
+++ src/com/tictactoe/GameIndex.java
@@ -0,0 +1,61 @@
+package com.tictactoe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class GameIndex extends Activity {
+
+	private Button newGame, exit, credits;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_index);
+		
+		newGame = (Button) findViewById(R.id.newGame);
+		exit = (Button) findViewById(R.id.exit);
+		credits = (Button) findViewById(R.id.credits);
+		
+		newGame.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View vNewGame) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent("com.thesportingapp.NEW");
+				startActivity(i);
+			}
+		});
+		
+		exit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View vExit) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent("com.thesportingapp.EXIT");
+				startActivity(i);
+			}
+		});
+
+		credits.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View vCredits) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent("com.thesportingapp.CREDITS");
+				startActivity(i);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_game_index, menu);
+		return true;
+	}
+
+}
---
+++ src/com/tictactoe/NewGame.java
@@ -0,0 +1,16 @@
+package com.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewGame extends Activity{
+
+	@Override
+	protected void onCreate(Bundle newgame) {
+		// TODO Auto-generated method stub
+		super.onCreate(newgame);
+		setContentView(R.layout.newgame);
+	}
+	
+
+}
