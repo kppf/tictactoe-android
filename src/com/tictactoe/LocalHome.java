ae63843cc9faef33fb8064f2e2cc6ed334e67dcb
Kushagra Pandey <kushagra4296@gmail.com>
2013-02-25 16:51:30 +0530

    Added mainboard, touch listener and few more things

--- src/com/tictactoe/LocalHome.java
+++ src/com/tictactoe/LocalHome.java
@@ -2,17 +2,20 @@ package com.tictactoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 
 public class LocalHome extends Activity {
-	
+	private LocalHomeCanvas mycanvas;
 	protected void onCreate(Bundle b) {
 		System.out.println("In LocalHome class");
 		super.onCreate(b);
 		setContentView(R.layout.localhome);
-		LocalHomeCanvas mycanvas = (LocalHomeCanvas)findViewById(R.id.localHomeCanvas);
+		mycanvas = (LocalHomeCanvas)findViewById(R.id.localHomeCanvas);
 		mycanvas.setOnClickListener(clickListener);
+		mycanvas.setOnTouchListener(touchListener);
     }
     
     private OnClickListener clickListener = new OnClickListener() {
@@ -21,6 +24,30 @@ public class LocalHome extends Activity {
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			
+		}
+	};
+	
+	private OnTouchListener touchListener = new OnTouchListener() {
+
+		@Override
+		public boolean onTouch(View arg0, MotionEvent event) {
+			// TODO Auto-generated method stub
+			int x = (int)event.getX();
+		    int y = (int)event.getY();
+		    boolean touch = false;
+		    switch (event.getAction()) {
+		        case MotionEvent.ACTION_DOWN:	touch=false; break;
+		        case MotionEvent.ACTION_MOVE:	break;
+		        case MotionEvent.ACTION_UP:		touch=true; break;
+		    } 
+		    System.out.println(x+" "+y);
+		    if(touch==true) 
+		    {
+		    	System.out.println("Touch lifted.");
+		    	if(mycanvas.play(x,y)==true) mycanvas.putImage(x,y);
+		    	touch=false;
+		    }
+			return false;
 		}
 	};
 }
--- src/com/tictactoe/LocalHomeCanvas.java
+++ src/com/tictactoe/LocalHomeCanvas.java
@@ -36,6 +36,8 @@ public class LocalHomeCanvas extends SurfaceView implements Callback {
 */
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -43,23 +45,44 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class LocalHomeCanvas extends View {
-
+	private Bitmap kBitmap = null, cross=null, round=null;
 	private Paint kPaint = null;
-	private int MAXX, MAXY;
+	private int MAXX, MAXY, MBH=250, MBW=250;
 	public LocalHomeCanvas(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
 		kPaint = new Paint();
 		kPaint.setColor(Color.BLUE);
-	}
+		kBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mainboard);
+		cross = BitmapFactory.decodeResource(getResources(), R.drawable.cross);
+		round = BitmapFactory.decodeResource(getResources(), R.drawable.round);
+		}
 	
 	public void onDraw(Canvas canvas) {
 		MAXY = getHeight();
 		MAXX = getWidth();
-		canvas.drawRGB(0, 0, 255);
+	//	canvas.drawRGB(0, 0, 255);
 	//	canvas.drawRect(0, 0, MAXX/2, MAXY/2, kPaint);
 		kPaint.setColor(Color.RED);
-		canvas.drawLine(MAXX/3, MAXY/12, MAXX/3, MAXY/12+MAXX, kPaint);
+	//	canvas.drawLine(MAXX/3, MAXY/12, MAXX/3, MAXY/12+MAXX, kPaint);
 		System.out.println(MAXX+" "+MAXY);
+	//	canvas.setBitmap(kBitmap);
+		canvas.drawBitmap(kBitmap, MAXX/2-MBW/2, MAXY/2-MBH/2, kPaint);
+		System.out.println("Yo!");
+	}
+	void putImage(int x, int y) {
+		// TODO Auto-generated method stub
+		System.out.println("Why you no put image here :D");
+	}
+
+	//We can implement something like getRelativeX getRelativeY to direct get the matrix position of the touch
+	
+	boolean play(int x, int y) {
+		// TODO Auto-generated method stub
+		int kx = x - (MAXX/2-MBW/2);
+		int ky = y - (MAXY/2-MBH/2);
+		System.out.println(kx+" KKK "+ky);
+		if(kx>0 && ky>0 && kx<MBW && ky<MBH) return true;
+		return false;
 	}
 }
