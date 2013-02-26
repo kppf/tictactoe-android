abad2cf32aab120478891cd4b9947819eeb9515d
Kushagra Pandey <kushagra4296@gmail.com>
2013-02-27 01:00:43 +0530

    Phase 1 done! Can play two player game easily

--- src/com/tictactoe/LocalHome.java
+++ src/com/tictactoe/LocalHome.java
@@ -6,18 +6,44 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LocalHome extends Activity {
 	private LocalHomeCanvas mycanvas;
+	private TextView label;
+	private Button button;
 	protected void onCreate(Bundle b) {
 		System.out.println("In LocalHome class");
 		super.onCreate(b);
 		setContentView(R.layout.localhome);
 		mycanvas = (LocalHomeCanvas)findViewById(R.id.localHomeCanvas);
+		button = (Button) findViewById(R.id.the_button);
+		label = (TextView) findViewById(R.id.the_label);
 		mycanvas.setOnClickListener(clickListener);
 		mycanvas.setOnTouchListener(touchListener);
+		button.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				mycanvas.restart();
+				button.setText(mycanvas.restart);
+				label.setText(mycanvas.playerR);
+			}
+			
+		});
     }
     
+	private void showWin() {
+		label.setText(mycanvas.showWin());
+		button.setText(mycanvas.playAgain);
+	}
+	
+	private void showDraw() {
+		label.setText(mycanvas.showDraw());
+		button.setText(mycanvas.playAgain);
+	}
+	
     private OnClickListener clickListener = new OnClickListener() {
 	//	private boolean isDrawn = false;
 		@Override
@@ -45,6 +71,11 @@ public class LocalHome extends Activity {
 		    {
 		    	System.out.println("Touch lifted.");
 		    	if(mycanvas.play(x,y)==true) mycanvas.putImage(x,y);
+				boolean win = mycanvas.checkWin();
+				boolean draw = mycanvas.checkDraw();
+				if(win==true) showWin();
+				else if(draw==true) showDraw();
+				else label.setText(mycanvas.currentPlayer());
 		    	touch=false;
 		    }
 			return false;
--- src/com/tictactoe/LocalHomeCanvas.java
+++ src/com/tictactoe/LocalHomeCanvas.java
@@ -43,12 +43,17 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LocalHomeCanvas extends View {
 	private Bitmap kBitmap = null, cross=null, round=null;
 	private Paint kPaint = null;
 	private int MAXX, MAXY, MBH=227, MBW=207, kx, ky, CW=50, CH=50, RH=50, RW=50, MARX, MARY;
 	private int mat[][], CI=-1, RI=1, DI=0, player;
+	private boolean isOver=false;
+	protected String playerR = "Player 1", playerC="Player 2", drawString ="Game Drawn!", winString=" wins!";
+	protected String playAgain="Play Again!", restart="Restart";
 /*
  * CI->Cross index, RI-> Round Index, DI->Default index, player->current player
  * MBH-> main board height, MBW->width, CW,Ch->Cross, RH, RW->round
@@ -71,30 +76,58 @@ public class LocalHomeCanvas extends View {
 		MAXX = getWidth();
 		MARX = MAXX/2-MBW/2;
 		MARY = MAXY/2-MBH/2;
-	//	canvas.drawRGB(0, 0, 255);
-	//	canvas.drawRect(0, 0, MAXX/2, MAXY/2, kPaint);
 		kPaint.setColor(Color.RED);
-	//	canvas.drawLine(MAXX/3, MAXY/12, MAXX/3, MAXY/12+MAXX, kPaint);
 		System.out.println(MAXX+" "+MAXY);
-	//	canvas.setBitmap(kBitmap);
 		canvas.drawBitmap(kBitmap, MARX, MARY, kPaint);
 		System.out.println("Yo!");
-		int win = checkWin();
 		for(int i=0; i<3; i++) for(int j=0; j<3; j++) {
 			if(mat[i][j]==CI) canvas.drawBitmap(cross,MARX+(2*i+1)*MBW/6-CW/2,MARY+(2*j+1)*MBH/6-CH/2,kPaint);
 			else if(mat[i][j]==RI) canvas.drawBitmap(round,MARX+(2*i+1)*MBW/6-RW/2,MARY+(2*j+1)*MBH/6-RH/2,kPaint);
 		}
+		if(checkDraw()==true || checkWin()==true) isOver=true;
 	}
 	
-	private int checkWin() {
+	protected String currentPlayer() {
+		if(player==RI) return playerR;
+		else return playerC;
+	}
+	
+	protected String showDraw() {
+		return(drawString);
+	}
+
+	protected String showWin() {
+		String winPlayer;
+		if(player==CI) winPlayer=playerR;
+		else winPlayer=playerC;
+		return(winPlayer+winString);
+	}
+
+	protected boolean checkDraw() {
+		for(int i=0; i<3; i++) for(int j=0; j<3; j++) if(mat[i][j]==DI) return false;
+		return true;
+	}
+	
+	protected boolean checkWin() {		//canvas may be passed as argument and line may be shown cutting the similar ones or just an image of line may be added
 		// TODO Auto-generated method stub
+		int checkPlayer;
+		if(player==RI) checkPlayer=CI;
+		else checkPlayer=RI;
 		
+		if((mat[0][0]==checkPlayer && mat[0][0]==mat[1][1] && mat[1][1]==mat[2][2])||(checkPlayer == mat[2][0] && mat[2][0]==mat[1][1] && mat[1][1]==mat[0][2])) {
+            return true;
+        }
+        for(int i=0; i<3; i++)
+            if((mat[i][0]==checkPlayer && mat[i][0]==mat[i][1] && mat[i][1]==mat[i][2])||(mat[0][i]==checkPlayer && mat[0][i]==mat[1][i] && mat[1][i]==mat[2][i])) {
+                return true;
+            }
+		return false;
 	}
 
 	void putImage(int x, int y) {
 		// TODO Auto-generated method stub
 		System.out.println("Why you no put image here :D");
-		if(mat[kx/(MBW/3)][ ky/(MBH/3)]==DI) {
+		if(mat[kx/(MBW/3)][ky/(MBH/3)]==DI && isOver==false) {
 		if(player==RI)
 		{
 			mat[kx/(MBW/3)][ ky/(MBH/3)]=RI;
@@ -117,5 +150,12 @@ public class LocalHomeCanvas extends View {
 		System.out.println(kx+" KKK "+ky);
 		if(kx>0 && ky>0 && kx<MBW && ky<MBH) return true;
 		return false;
+	}
+	
+	protected void restart() {
+		for(int i=0; i<3; i++) for(int j=0; j<3; j++) mat[i][j]=DI;
+		player=RI;
+		isOver=false;
+		invalidate();
 	}
 }
